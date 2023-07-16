360e6c077e1b7e0c64b63e1380abf5e5bf405690
pravedens <107057525+pravedens@users.noreply.github.com>
2023-07-16 11:56:28 +0500

    Предоставление обмена в соц сетях

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -6,7 +6,7 @@ const route = useRoute();
 const { data, error } = await useFetch(
   `https://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
   {
-    pick: ["Plot", "Title", "Error"],
+    pick: ["Plot", "Title", "Error", "Poster"],
     key: `/movies/${route.params.id}`,
   }
 );
@@ -16,6 +16,15 @@ if (error.value) {
 if (data.value.Error === "Incorrect IMDb ID.") {
   showError({ statusCode: 404, statusMessage: "Page Not Found" });
 }
+useHead({
+  title: data.value.Title,
+  meta: [
+    { name: "description", content: data.value.Plot },
+    { property: "og:description", content: data.value.Plot },
+    { property: "og:image", content: data.value.Poster },
+    { name: "twitter:card", content: `summary_large_image` },
+  ],
+});
 </script>
 
 <template>
