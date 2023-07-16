d0686e9088d20fc5fb08eeea96def2a748c88bed
pravedens <107057525+pravedens@users.noreply.github.com>
2023-07-16 10:43:54 +0500

    Дедупликация асинхронных данных с помощью ключа

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 const route = useRoute();
-const { data } = await useAsyncData(() => {
+const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
   return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
 });
 </script>
