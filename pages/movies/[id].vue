934ba30ac718ba4b4d55dd91545b81f424196267
pravedens <107057525+pravedens@users.noreply.github.com>
2023-07-16 11:31:15 +0500

    Обработка ошибок useFetch

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,12 +1,21 @@
 <script lang="ts" setup>
 const route = useRoute();
-const { data } = useFetch(
-  `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
+
+// {"Response":"False","Error":"Incorrect IMDb ID."}
+
+const { data, error } = await useFetch(
+  `https://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
   {
-    pick: ['Plot', 'Title'],
+    pick: ["Plot", "Title", "Error"],
     key: `/movies/${route.params.id}`,
   }
 );
+if (error.value) {
+  // handle accordingly
+}
+if (data.value.Error === "Incorrect IMDb ID.") {
+  showError({ statusCode: 404, statusMessage: "Page Not Found" });
+}
 </script>
 
 <template>
